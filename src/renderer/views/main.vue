3562e2bf07c7fd15f5f4c3ab38a36b31491b4973
xiewen <xiewen@asiainfo.com>
2018-10-23 19:12:32 +0800

    add build

--- src/renderer/views/main.vue
+++ src/renderer/views/main.vue
@@ -70,9 +70,11 @@
             //注意：“downloadProgress”事件可能存在无法触发的问题，只需要限制一下下载网速就好了
             ipcRenderer.on("downloadProgress", (event, progressObj)=> {
                 console.log(progressObj);
-                this.downloadPercent = progressObj.percent || 0;
+                this.dialogDownloadProgressVisible = true;
+                this.downloadPercent = progressObj.percent.toFixed(2) || 0;
             });
             ipcRenderer.on("isUpdateNow", () => {
+                // 强制更新
                 ipcRenderer.send("isUpdateNow");
             });
         },
